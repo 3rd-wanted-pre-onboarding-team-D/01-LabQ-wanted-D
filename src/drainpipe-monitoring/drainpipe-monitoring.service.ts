186905439125277a82336562305ea14702160b94
areum <reumaco99@gmail.com>
2022-06-30 13:25:00 +0900

    Feat: 데이터 총 개수에 따른 정보 조회 관련 추가
    데이터 조회시 요청값으로 첫번째 인덱스와 마지막 인덱스를 기본설정을 해둔 상태로 전체 데이터 조회가 안됐었음.
    
    <Service>
    -getDataTotalCount() 데이터 행 전체 개수 구하기 추가
    -rowData() 데이터에서 행만 가져오기 추가
    -getApiData() api 데이터 가져오기 추가
    
    <Dto>
    -Dto폴더 추가하고 dto 파일 이동
    -DrainpipeInfoOpenApiDto 클래스 추가
    -RequestDrainpipeInfoDto 변수명 대문자로 변경
    -ResponseDrainpipeInfoDto 변수명 대문자로 변경
    
    Resolves: #4

--- src/drainpipe-monitoring/drainpipe-monitoring.service.ts
+++ src/drainpipe-monitoring/drainpipe-monitoring.service.ts
@@ -3,6 +3,9 @@ import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { firstValueFrom, map } from 'rxjs';
 import { ResponseDrainpipeInfoDto } from './response.drainpipe-info.dto';
+import { DrainpipeInfoOpenApiDto } from './dto/drainpipe-info-openapi.dto';
+import { start } from 'repl';
+import { Any } from 'typeorm';
 
 @Injectable()
 export class DrainpipeMonitoringService {
@@ -11,47 +14,82 @@ export class DrainpipeMonitoringService {
     private readonly configService: ConfigService,
   ) {}
 
-  // : Promise<AxiosResponse<getDrainpipeInfoDto[]>>
-  async getDrainpipeApi(reqGubn, reqYmd, reqYmd2): Promise<ResponseDrainpipeInfoDto[]> {
-    const key = this.configService.get<string>('OPEN_API_KEY');
+  //행 전체 개수 구하기
+  async getDataTotalCount(reqGubn, reqYmd, reqYmd2) {
+    const data = await this.getApiData(1, 1, reqGubn, reqYmd, reqYmd2);
+    return data.DrainpipeMonitoringInfo.list_total_count;
+  }
 
-    const testUrl = `http://openapi.seoul.go.kr:8088/${key}/json/DrainpipeMonitoringInfo/1/5/01/2022030214/2022030315`;
-    const url = `http://openapi.seoul.go.kr:8088/${key}/json/DrainpipeMonitoringInfo/1/5/${reqGubn}/${reqYmd}/${reqYmd2}`;
+  async getDrainpipeApi(
+    reqGubn,
+    reqYmd,
+    reqYmd2,
+  ): Promise<ResponseDrainpipeInfoDto[]> {
+    //데이터 총 개수
+    const totalDataCount = await this.getDataTotalCount(
+      reqGubn,
+      reqYmd,
+      reqYmd2,
+    );
 
-    try {
-      const responseData = await firstValueFrom(
-        this.httpService.get(url).pipe(map((response) => response.data)),
+    //리턴할 새 배열 생성
+    const responseArr = [];
+
+    //최대 1000건 요청을 위해 데이터 자르기
+    const repeat = totalDataCount / 1000;
+
+    //1000개 단위 데이터 요청
+    for (let i = 0; i <= repeat; i++) {
+      const getData = await this.rowData(
+        i * 1000 + 1,
+        (i + 1) * 1000,
+        reqGubn,
+        reqYmd,
+        reqYmd2,
       );
 
-      //responseData
-      // {
-      //   DrainpipeMonitoringInfo: {
-      //     list_total_count: 6240,
-      //     RESULT: { CODE: 'INFO-000', MESSAGE: '정상 처리되었습니다' },
-      //     row: [ [Object], [Object], [Object], [Object], [Object] ] //row안에 있는 값에 접근해야함
-      //   }
-      // }
-
-      const rowData = responseData.DrainpipeMonitoringInfo.row;
-
-      const drainpipeDto = new ResponseDrainpipeInfoDto();
-
-      const responseArr = [];
-
-      for (const idx in rowData) {
-        drainpipeDto.idn = rowData[idx].IDN;
-        drainpipeDto.gubn = rowData[idx].GUBN;
-        drainpipeDto.gubn_nam = rowData[idx].GUBN_NAM;
-        drainpipeDto.mea_ymd = rowData[idx].MEA_YMD;
-        drainpipeDto.mea_wal = rowData[idx].MEA_WAL;
-        drainpipeDto.sig_sta = rowData[idx].SIG_STA;
-
-        responseArr.push(drainpipeDto);
+      for (const idx in getData) {
+        responseArr.push(getData[idx]);
       }
-
-      return responseArr;
-    } catch (e) {
-      NotFoundException;
     }
+
+    //1000개 단위로 나눈 나머지 요청
+    if (totalDataCount < repeat * 1000) {
+      const getData = await this.rowData(
+        repeat * 1000 + 1,
+        totalDataCount,
+        reqGubn,
+        reqYmd,
+        reqYmd2,
+      );
+
+      for (const idx in getData) {
+        responseArr.push(getData[idx]);
+      }
+    }
+
+    return responseArr;
+  }
+
+  //각 행 가져오기
+  async rowData(startIdx, lastIdx, reqGubn, reqYmd, reqYmd2) {
+    const data = await (
+      await this.getApiData(startIdx, lastIdx, reqGubn, reqYmd, reqYmd2)
+    ).DrainpipeMonitoringInfo.row;
+    return data;
+  }
+
+  //OPEN API 데이터 가져오기
+  async getApiData(startIdx, lastIdx, reqGubn, reqYmd, reqYmd2) {
+    const key = this.configService.get<string>('OPEN_API_KEY');
+    //const testUrl = `http://openapi.seoul.go.kr:8088/${key}/json/DrainpipeMonitoringInfo/1/5/01/2022030214/2022030315`;
+    const url = `http://openapi.seoul.go.kr:8088/${key}/json/DrainpipeMonitoringInfo/${startIdx}/${lastIdx}/${reqGubn}/${reqYmd}/${reqYmd2}`;
+
+    const responseData = await firstValueFrom(
+      this.httpService
+        .get<DrainpipeInfoOpenApiDto>(url)
+        .pipe(map((response) => response.data)),
+    );
+    return responseData;
   }
 }
